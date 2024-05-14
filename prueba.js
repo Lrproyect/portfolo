b3ae92fd7acf319e2d6b8e060247752fc28e76b8
Lrproyect <127346585+Lrproyect@users.noreply.github.com>
2024-05-14 11:51:16 -0300

    prueba.js

--- prueba.js
+++ prueba.js
@@ -1,5 +1,7 @@
 function openInSameTab(event) {
-    event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-    window.location.href = event.currentTarget.parentElement.href; // Abrir la URL del enlace en la misma pestaña
+  event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+  var anchor = event.currentTarget.closest('a'); // Obtener el elemento 'a' más cercano al elemento 'img' actual
+  if (anchor) {
+    window.location.href = anchor.href; // Abrir la URL del enlace en la misma pestaña
   }
-  
+}
