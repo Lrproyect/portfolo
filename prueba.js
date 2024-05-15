c5ee77db4866fb28b6cf431805807366da6fb2fb
Lrproyect <127346585+Lrproyect@users.noreply.github.com>
2024-05-15 10:25:36 -0300

    Delete prueba.js

--- prueba.js
+++
@@ -1,7 +0,0 @@
-function openInSameTab(event) {
-  event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
-  var anchor = event.currentTarget.closest('a'); // Obtener el elemento 'a' más cercano al elemento 'img' actual
-  if (anchor) {
-    window.location.href = anchor.href; // Abrir la URL del enlace en la misma pestaña
-  }
-}
